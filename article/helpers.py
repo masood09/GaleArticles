697084e573041ecee74ba04d036f860f50350a38
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 13:14:24 +0530

    Displaying the list of articles in home page.

--- article/helpers.py
+++ article/helpers.py
@@ -7,3 +7,10 @@ def helper_get_random_article():
     ).order_by(
         '?'
     ).first()
+
+def helper_get_published_article():
+    return Article.objects.exclude(
+        publication_date__gt=datetime.date.today()
+    ).all().order_by(
+        'publication_date'
+    )
--- article/views.py
+++ article/views.py
@@ -5,11 +5,13 @@ from .helpers import *
 
 def listing(request):
     random_article = helper_get_random_article()
+    article_list = helper_get_published_article()
 
     return render(
         request,
         'list.html',
         {
-            'random_article': random_article
+            'random_article': random_article,
+            'article_list' : article_list,
         }
     )
