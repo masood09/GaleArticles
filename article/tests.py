ed5492b8375e3a027e09b723cafdfbfdd962ba95
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 18:56:20 +0530

    Correcting the test cases

--- article/tests.py
+++ article/tests.py
@@ -40,15 +40,15 @@ class ResponseCodeTest(TestCase):
 
     def test_detail_page_proper(self):
         """When the page exists and is published (we are using fixtures to seed the data) we should get 200"""
-        response = self.client.get('/blog/18/')
+        response = self.client.get('/blog/omnia-peccata-paria-dicitis/')
         self.assertEquals(response.status_code, 200)
 
     def test_detail_page_not_published(self):
         """When the page is not published yet (ie., publishing date is in future), we need to send 404 response"""
-        response = self.client.get('/blog/25/')
+        response = self.client.get('/blog/si-quicquam-extra-virtutem-habeatur/')
         self.assertEquals(response.status_code, 404)
 
     def test_detail_page_not_exist(self):
         """When the page does not exist, visiting the page should return 404"""
-        response = self.client.get('/blog/1000/')
+        response = self.client.get('/blog/asdf/')
         self.assertEquals(response.status_code, 404)
