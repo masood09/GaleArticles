c05d0c7bb70efba4c153f7b21a7e816a067e163c
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 07:36:34 +0530

    Making sure we get the proper string representation for Category model.

--- article/models.py
+++ article/models.py
@@ -6,3 +6,7 @@ class Category(models.Model):
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        """We want the string representation of Category model to be its title"""
+        return self.title
