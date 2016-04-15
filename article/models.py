e47144717f20c2a06e43c07a1107f4c8ada9deca
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 08:02:36 +0530

    Creating the model Article which will represent our articles.

--- article/models.py
+++ article/models.py
@@ -14,3 +14,16 @@ class Category(models.Model):
     class Meta:
         """Django will call this as Categorys, lets make it grammatically correct!"""
         verbose_name_plural = "categories"
+
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('auth.User')
+    publication_date = models.DateField()
+    category = models.ForeignKey(Category)
+    hero_image = models.ImageField(upload_to='images/')
+    optional_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    body_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
