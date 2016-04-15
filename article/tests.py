816a52df8e7becab55250af13f16190f13244643
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 07:35:08 +0530

    Adding test case to check whether we get the proper string representation for Category model.

--- article/tests.py
+++ article/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoryModelTest(TestCase):
+    """The unit test cases for Category Model"""
+
+    def test_string_representation(self):
+        '''Test whether the string representation is equal to its title'''
+        category = Category(title="My Category Title")
+        self.assertEquals(str(category), category.title)
