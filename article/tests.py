a1c1e4665215fd34916acafefc516d11bf050196
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 08:05:30 +0530

    Writing test to make sure that the string representation of Article model is equal to its title.

--- article/tests.py
+++ article/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from .models import Category
+from .models import Article
 
 
 class CategoryModelTest(TestCase):
@@ -14,3 +15,13 @@ class CategoryModelTest(TestCase):
     def test_category_name_plural(self):
         """Test whether the plural name is correct"""
         self.assertEquals(str(Category._meta.verbose_name_plural), "categories")
+
+
+
+class ArticleModelTest(TestCase):
+    """The unit test cases for Article Model"""
+
+    def test_string_representation(self):
+        '''Test whether the string representation is equal to its title'''
+        article = Article(title="My Article Title")
+        self.assertEquals(str(article), article.title)
