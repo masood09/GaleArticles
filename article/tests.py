e9505d3e24cb7f08d60cfa11195e693539270d12
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-15 07:37:51 +0530

    Writing test case to make sure the pluralized text of Category is Categories.

--- article/tests.py
+++ article/tests.py
@@ -10,3 +10,7 @@ class CategoryModelTest(TestCase):
         '''Test whether the string representation is equal to its title'''
         category = Category(title="My Category Title")
         self.assertEquals(str(category), category.title)
+
+    def test_category_name_plural(self):
+        """Test whether the plural name is correct"""
+        self.assertEquals(str(Category._meta.verbose_name_plural), "categories")
