3330bd1312302249c828d8886e4c58ea8734d305
Masood Ahmed <masood.ahmed09@gmail.com>
2016-04-21 20:05:32 +0530

    Implementing article details page using Vue.js

--- static/js_src/init.js
+++ static/js_src/init.js
@@ -12,3 +12,39 @@ $(document).ready(function(){
 
     $('.slick__carousel').slick();
 });
+
+Vue.config.devtools = false;
+Vue.config.delimiters = ['${', '}'];
+Vue.config.unsafeDelimiters = ['{!!', '!!}'];
+
+article__detail = {
+    init: function() {
+        new Vue({
+            el: '#vue__container',
+
+            data: {
+                article: [],
+            },
+
+            ready: function() {
+                this.$http({url: jQuery('#api__article_detail_url').val(), method: 'GET'}).then(function (response) {
+                    this.$set('article', response.data)
+                }, function (response) {
+                    console.log(response);
+                });
+            },
+
+            computed: {
+                body_text_linebreaks: function () {
+                    return this.article.body_text.replace(/(?:\r\n|\r|\n)/g, '<br>');
+                },
+
+                hero_style: function() {
+                    return {
+                        backgroundImage: 'url(\'' + this.article.hero_image + '\')'
+                    };
+                }
+            }
+        });
+    }
+};
